2e6a98e4712f92b7e4764c158c46b4c1eabb325e
AndreasChristensen89 <brogaard89@gmail.com>
2024-09-09 13:02:52 +0000

    lift state

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
+import Log from "./components/Log.jsx";
 import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
-  
+  const [ gameTurns, setGameTurns ] = useState([]);
+  const [ activePlayer, setActivePlayer ] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => curActivePlayer=== "X" ? "O" : "X");
+    setGameTurns();
+  }
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="PLayer 1" symbol="X" />
-          <Player initialName="PLayer 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="PLayer 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player initialName="PLayer 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
 
-        GAME BOARD
+        <GameBoard activePlayerSymbol={activePlayer} onSelectSquare={handleSelectSquare} />
       </div>
+      <Log />
     </main>
   );
 }
