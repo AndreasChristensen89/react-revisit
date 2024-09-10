efe4e126ef11ef080c213838f2098276bab53028
AndreasChristensen89 <brogaard89@gmail.com>
2024-09-10 11:43:48 +0000

    Add winner logic

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -2,20 +2,63 @@ import { useState } from "react";
 import Log from "./components/Log.jsx";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+
+// helper function
+function derivedActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
+
+const initialGameBoard = [
+  [null,null,null],
+  [null,null,null],
+  [null,null,null]
+]
 
 function App() {
   const [ gameTurns, setGameTurns ] = useState([]);
-  const [ activePlayer, setActivePlayer ] = useState("X");
+
+  const activePlayer = derivedActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+      const { square, player} = turn;
+      const { row, col } = square;
+
+      gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = 
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = 
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = 
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol && 
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer=== "X" ? "O" : "X");
-    setGameTurns(prevTurns => {
-      let currentPlayer = "X";
+    setGameTurns((prevTurns) => {
+      const currentPlayer = derivedActivePlayer(prevTurns);
 
-      if (prevTurns.length > 0 && prevTurns[0] === "X") {
-        currentPlayer = "O";
-      }
-      const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: activePlayer }, ...prevTurns];
+      const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns];
 
       return updatedTurns;
     });
@@ -28,10 +71,10 @@ function App() {
           <Player initialName="PLayer 1" symbol="X" isActive={activePlayer === "X"} />
           <Player initialName="PLayer 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {winner && <p>You won, {winner}!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
