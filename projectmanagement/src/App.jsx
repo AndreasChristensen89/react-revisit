ba0a96391f0253cb9b01db58e3259b4b5ae6a06f
AndreasChristensen89 <brogaard89@gmail.com>
2024-09-23 12:38:35 +0000

    start styling new app and create logic

--- projectmanagement/src/App.jsx
+++ projectmanagement/src/App.jsx
@@ -1,8 +1,30 @@
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
+
+import { useState } from "react";
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projectsArray: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <NoProjectSelected onStartAddProject={handleStartAddProject} />
+      
+    </ main>
   );
 }
 
